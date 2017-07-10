bfe5756b934f971cd1b1a317eeafb9ece4b7e946
Minshall, Olivia <olivia.minshall@capgemini.com>
2017-07-10 17:39:06 +0100

    added files

--- web/controllers/userController.js
+++ web/controllers/userController.js
@@ -5,7 +5,16 @@ exports.user_create_get = function(req, res) {
     res.render('user_registration', { title: 'Create User' });
 };
 
+// Display Author create form on GET
+exports.user_login_get = function(req, res) {
+    res.render('login', { title: 'Login' });
+};
+
 // Handle Author create on POST
 exports.user_create_post = function(req, res) {
     res.send('NOT IMPLEMENTED: User create POST');
+};
+// Handle Author login on POST
+exports.user_login_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Login POST');
 };
