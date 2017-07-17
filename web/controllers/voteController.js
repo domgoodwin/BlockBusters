7ec7dc3efc92f7ec82d744983029a350b1a9d5be
Dom Goodwin <dom.goodwin@capgemini.com>
2017-07-17 18:40:22 +0100

    fixed hex issue

--- web/controllers/voteController.js
+++ web/controllers/voteController.js
@@ -33,9 +33,23 @@ exports.user_displayvote_post = function(req, res) {
     var blockid = req.body.blockid;
     ch.getVote("july-2017", blockid, function(cb){
         //res.send('VOTE CAST: ' + cb.toString());
-        res.render('retrievevote', { info: cb.toString()});
+        console.log(cb);
+        var info = cb.toString();
+        info = info.replace('"', '');
+        info = info.replace('"', '');
+        console.log(info);
+        var content = hex2a(info);
+        console.log(content);
+        var cont = JSON.parse(content);
+        res.render('retrievevote', { info: cont.decision});
     });
 };
+
+function hex2a(hex) {
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2) str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
 
 exports.user_retrievevote_get = function(req, res) {
     res.render('retrievevote', { title: 'Vote'});
